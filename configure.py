bd524505bfd6c1d0e9a42330d67a1dde74298a9f
Matt Venn <matt@mattvenn.net>
2023-05-25 14:57:16 +0200

    fix problem with wokwi files not being fetched. Add the wokwi truthtable testing that got lost in merge

--- configure.py
+++ configure.py
@@ -99,11 +99,6 @@ class Projects():
             project.post_clone_setup()
             logging.debug(project)
 
-            # fetch the wokwi source
-            if args.clone_all:
-                if not project.is_fill() and project.is_wokwi():
-                    project.fetch_wokwi_files()
-
             if args.harden:
                 if filler is False:
                     project.create_user_config()
