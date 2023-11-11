f362ebcec24953aaa97793ba130dccdccf1b2c87
Uri Shaked <uri@urishaked.com>
2023-11-11 22:47:47 +0200

    ci(precheck): speed up precheck tests

--- precheck/default.nix
+++ precheck/default.nix
@@ -1,8 +1,4 @@
-{ pkgs ? import
-    (builtins.fetchGit {
-      url = "https://github.com/NixOS/nixpkgs.git";
-      rev = "f2bd8adf7b78d7616b52d0ef08865c7c2fcf189d";
-    })
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f2bd8adf7b78d7616b52d0ef08865c7c2fcf189d.tar.gz")
     { }
 , magic ? import ./nix/magic.nix { inherit pkgs; }
 ,
