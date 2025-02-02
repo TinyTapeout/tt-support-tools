0a5eadaf88a9d4db32564c2a5b1c3999a187ed52
Uri Shaked <uri@urishaked.com>
2025-02-02 10:48:37 +0200

    style(rom): format verilog code with verible

--- rom/tt_um_chip_rom.v
+++ rom/tt_um_chip_rom.v
@@ -10,28 +10,28 @@
 `default_nettype none
 
 `ifndef ROM_VMEM_PATH
-    `define ROM_VMEM_PATH "rom.vmem"
+`define ROM_VMEM_PATH "rom.vmem"
 `endif
 
 module tt_um_chip_rom (
-	input  wire [7:0] ui_in,	// Dedicated inputs
-	output wire [7:0] uo_out,	// Dedicated outputs
-	input  wire [7:0] uio_in,	// IOs: Input path
-	output wire [7:0] uio_out,// IOs: Output path
-	output wire [7:0] uio_oe,	// IOs: Enable path (active high: 0=input, 1=output)
-	input  wire       ena,
-	input  wire       clk,
-	input  wire       rst_n
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-	reg [7:0] rom_data [0:255];
+  reg [7:0] rom_data[0:255];
 
   initial begin
-		$readmemh(`ROM_VMEM_PATH, rom_data);
-	end
-	
-	assign uo_out  = rom_data[ui_in];
-	assign uio_out = 8'h00;
-	assign uio_oe  = 8'h00;
+    $readmemh(`ROM_VMEM_PATH, rom_data);
+  end
 
-endmodule // tt_um_chip_rom
+  assign uo_out  = rom_data[ui_in];
+  assign uio_out = 8'h00;
+  assign uio_oe  = 8'h00;
+
+endmodule  // tt_um_chip_rom
