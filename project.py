08071b0c014950bd51743023c710dae602fd7ba5
Uri Shaked <uri@urishaked.com>
2025-09-17 11:56:14 +0300

    fix(tt_tool): always specify the power pin names
    Until this point, we relied on the PDK's default power pin names. However, the defaults names for GF180, VDD and VSS, are not compatible with Tiny Tapeout, which expects VDPWR and VGND. So instead, we set the VDD_PIN and GND_PIN config options explicitly.

--- project.py
+++ project.py
@@ -482,6 +482,8 @@ class Project:
             "VERILOG_FILES": [f"dir::{src}" for src in self.sources],
             "DIE_AREA": die_area,
             "FP_DEF_TEMPLATE": def_template,
+            "VDD_PIN": "VDPWR",
+            "GND_PIN": "VGND",
             "RT_MAX_LAYER": self.tech.project_top_metal_layer,
         }
         write_config(config, os.path.join(self.src_dir, "user_config"), ("json",))
